3b3befa81e6eb11cbacf2331a719d03e8e586765
JoelmaRegina <joelma_silvaa@hotmail.com>
2021-10-14 04:16:47 -0300

    Starting the responsive adaptation of the project

---
+++ HTML-CSS-Javascript/js/script.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById("menu");
+
+function mostraMenu (){
+    if(menu.style.display == "none"){
+        menu.style.display = "flex"
+    }else {
+        menu.style.display = "none"
+    }
+}
